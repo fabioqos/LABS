f04ef1e259e4b50cab015f5ab6e6fcf683a1c39c
Fabio <fabioqos@gmail.com>
2019-04-07 12:16:21 +1000

    index added

---
+++ website/index_files/loader-v2.js
@@ -0,0 +1,102 @@
+(function() {
+
+    var __hs_cta_json = {"css":"a#cta_button_330046_ff115ee4-2248-40d6-a58e-2c9eb96b9b6b {\n  cursor:pointer; \n}\na#cta_button_330046_ff115ee4-2248-40d6-a58e-2c9eb96b9b6b:hover {\n}\na#cta_button_330046_ff115ee4-2248-40d6-a58e-2c9eb96b9b6b:active, #cta_button_330046_ff115ee4-2248-40d6-a58e-2c9eb96b9b6b:active:hover {\n}\n\n","image_html":"<a id=\"cta_button_330046_ff115ee4-2248-40d6-a58e-2c9eb96b9b6b\" class=\"cta_button\" href=\"/cs/c/?cta_guid=ff115ee4-2248-40d6-a58e-2c9eb96b9b6b&placement_guid=05047d1a-c093-4d0b-850f-d629714b387a&portal_id=330046&canon=https%3A%2F%2Fwww.ansible.com%2F&redirect_url=APefjpHl3UxoO-z-Civ1pam6iMvE2p-agE5Vz0EEiOdeIxZ7kV-9mgD2JJLLqGf7MPKuHS8u2vreyqBMHjiW8ykSpepjzFeDmaFmbpk78qy51_HQjQjG4eCcQny_pd2v3THtnE3CHNTc_kSSCMvwq13GeBLexa_2SMsPGVj5mYwGdrxXkj-UwSku1OJMw3Jt1rWxyYIQJT5ewwDmj838KLYvuiAhm-v4aZPwxqgOIFGxZCFRkaORJEdNGkfR_yqxqANQL7e9K_27ecAvmSswxhKWGMUhJIYTatEfs4rjxZi55VrnEnbPjzGJaGYT-qmQVfj6aUBe7UnnDaOaWB_eB7FkGvLbui8PBiHs2Ubp9Pd2Yx-hYOGN6trXoRb_N6pFZJbfSsn5mm2SSLZR0A6yoZWxNF5WXxLrtA&click=a94f03b7-0ffd-4ea5-a431-ed1b8f39c265&hsutk=88591e2a7411b7dcda2640124521d21a&pageId=4617718409\"  cta_dest_link=\"https://www.ansible.com/tower-trial?utm_campaign=Ansible%20Global%20Nav&utm_source=Website&utm_medium=Tower%20Demo%20Video%20Global%20Nav%20Resources%20Link\"><img id=\"hs-cta-img-05047d1a-c093-4d0b-850f-d629714b387a\" class=\"hs-cta-img \" style=\"border-width: 0px; /*hs-extra-styles*/\" mce_noresize=\"1\" alt=\"Ansible Tower Demo\" src=\"https://cdn2.hubspot.net/hubshot/17/11/29/5b74dd09-aa74-4033-b24c-220e145197c2.png\" /></a>","is_image":false,"placement_element_class":"hs-cta-05047d1a-c093-4d0b-850f-d629714b387a","raw_html":"<a id=\"cta_button_330046_ff115ee4-2248-40d6-a58e-2c9eb96b9b6b\" class=\"cta_button \" href=\"/cs/c/?cta_guid=ff115ee4-2248-40d6-a58e-2c9eb96b9b6b&placement_guid=05047d1a-c093-4d0b-850f-d629714b387a&portal_id=330046&canon=https%3A%2F%2Fwww.ansible.com%2F&redirect_url=APefjpHl3UxoO-z-Civ1pam6iMvE2p-agE5Vz0EEiOdeIxZ7kV-9mgD2JJLLqGf7MPKuHS8u2vreyqBMHjiW8ykSpepjzFeDmaFmbpk78qy51_HQjQjG4eCcQny_pd2v3THtnE3CHNTc_kSSCMvwq13GeBLexa_2SMsPGVj5mYwGdrxXkj-UwSku1OJMw3Jt1rWxyYIQJT5ewwDmj838KLYvuiAhm-v4aZPwxqgOIFGxZCFRkaORJEdNGkfR_yqxqANQL7e9K_27ecAvmSswxhKWGMUhJIYTatEfs4rjxZi55VrnEnbPjzGJaGYT-qmQVfj6aUBe7UnnDaOaWB_eB7FkGvLbui8PBiHs2Ubp9Pd2Yx-hYOGN6trXoRb_N6pFZJbfSsn5mm2SSLZR0A6yoZWxNF5WXxLrtA&click=a94f03b7-0ffd-4ea5-a431-ed1b8f39c265&hsutk=88591e2a7411b7dcda2640124521d21a&pageId=4617718409\"  style=\"/*hs-extra-styles*/\" cta_dest_link=\"https://www.ansible.com/tower-trial?utm_campaign=Ansible%20Global%20Nav&utm_source=Website&utm_medium=Tower%20Demo%20Video%20Global%20Nav%20Resources%20Link\" title=\"Ansible Tower Demo\">Ansible Tower Demo</a>"};
+    var __hs_cta = {};
+
+    __hs_cta.drop = function() {
+        var is_legacy = document.getElementById('hs-cta-ie-element') && true || false,
+            html = __hs_cta_json.image_html,
+            tags = __hs_cta.getTags(),
+            is_image = __hs_cta_json.is_image,
+            parent, _style;
+
+        if (!is_legacy && !is_image) {
+            parent = (document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]);
+
+            _style = document.createElement('style');
+            parent.insertBefore(_style, parent.childNodes[0]);
+            try {
+                default_css = ".hs-cta-wrapper p, .hs-cta-wrapper div { margin: 0; padding: 0; }";
+                cta_css = default_css + " " + __hs_cta_json.css;
+                // http://blog.coderlab.us/2005/09/22/using-the-innertext-property-with-firefox/
+                _style[document.all ? 'innerText' : 'textContent'] = cta_css;
+            } catch (e) {
+                // addressing an ie9 issue
+                _style.styleSheet.cssText = cta_css;
+            }
+
+            html = __hs_cta_json.raw_html;
+
+            
+
+        }
+
+        for (var i = 0; i < tags.length; ++i) {
+
+            var tag = tags[i];
+            var images = tag.getElementsByTagName('img');
+            var cssText = "";
+            var align = "";
+            for (var j = 0; j < images.length; j++) {
+                align = images[j].align;
+                images[j].style.border = '';
+                images[j].style.display = '';
+                cssText = images[j].style.cssText;
+            }
+
+            if (align == "right") {
+                tag.style.display = "block";
+                tag.style.textAlign = "right";
+            } else if (align == "middle") {
+                tag.style.display = "block";
+                tag.style.textAlign = "center";
+            }
+
+            tag.innerHTML = html.replace('/*hs-extra-styles*/', cssText);
+            tag.style.visibility = 'visible';
+            tag.setAttribute('data-hs-drop', 'true');
+            window.hbspt && hbspt.cta && hbspt.cta.afterLoad && hbspt.cta.afterLoad('05047d1a-c093-4d0b-850f-d629714b387a');
+        }
+
+        return tags;
+    };
+
+    __hs_cta.getTags = function() {
+        var allTags, check, i, divTags, spanTags,
+            tags = [];
+            if (document.getElementsByClassName) {
+                allTags = document.getElementsByClassName(__hs_cta_json.placement_element_class);
+                check = function(ele) {
+                    return (ele.nodeName == 'DIV' || ele.nodeName == 'SPAN') && (!ele.getAttribute('data-hs-drop'));
+                };
+            } else {
+                allTags = [];
+                divTags = document.getElementsByTagName("div");
+                spanTags = document.getElementsByTagName("span");
+                for (i = 0; i < spanTags.length; i++) {
+                    allTags.push(spanTags[i]);
+                }
+                for (i = 0; i < divTags.length; i++) {
+                    allTags.push(divTags[i]);
+                }
+
+                check = function(ele) {
+                    return (ele.className.indexOf(__hs_cta_json.placement_element_class) > -1) && (!ele.getAttribute('data-hs-drop'));
+                };
+            }
+            for (i = 0; i < allTags.length; ++i) {
+                if (check(allTags[i])) {
+                    tags.push(allTags[i]);
+                }
+            }
+        return tags;
+    };
+
+    // need to do a slight timeout so IE has time to react
+    setTimeout(__hs_cta.drop, 10);
+    
+    window._hsq = window._hsq || [];
+    window._hsq.push(['trackCtaView', '05047d1a-c093-4d0b-850f-d629714b387a', 'ff115ee4-2248-40d6-a58e-2c9eb96b9b6b']);
+    
+
+}());
