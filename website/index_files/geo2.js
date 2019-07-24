61e3b9e0ae0f0f3d05218aa54c35e852a40ed19a
Fabio <fabioqos@gmail.com>
2019-07-24 14:00:30 +1000

    index added

---
+++ website/index_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "BRISBANE",
+      'continent': "OC",
+      'country': "AU",
+      'region': "QLD"
+    },
+    'ip':"66.187.239.16"
+  }]);
+})
+//
+()
+
+;
